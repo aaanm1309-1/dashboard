38ba911cea64d6b9c39b87b110ff0a7ff2703dd5
aaanm13 <aaanm1309.1@gmail.com>
2021-10-10 18:03:39 -0300

    final aula 11

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: 'light',
+  colors: {
+    primary: '#DCDCDC',
+    secondary: '#FFFFFF',
+    tertiary: '#f5f5f5',
+
+    white: '#000',
+    black: '#fff',
+    gray: '#BFBFBF',
+
+    success: '#03BB85',
+    info: '#3B5998',
+    warning: '#FF6961',
+
+  },
+};
