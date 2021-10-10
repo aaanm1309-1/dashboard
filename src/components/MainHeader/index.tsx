648db1fe7d47e5026740d2490a83ae61b8204f64
aaanm13 <aaanm1309.1@gmail.com>
2021-10-10 18:21:52 -0300

    final aula 12

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import { 
+  Container,
+  Profile,
+  Welcome,
+  UserName
+ } from './styles';
+
+ import emojis from '../../utils/emojis'
 
 const MainHeader: React.FC = () => {
+
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }
+  ,[])
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toggle</h1>
+      <Profile>
+          <Welcome>Olá, {emoji}</Welcome>
+          <UserName> Adriano Menezes</UserName>
+        </Profile>
     </Container>
   );
 };
